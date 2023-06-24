c0aafee0d2f0c5867375b006fb02ab6310a5fe77
road2rhodes <132181574+road2rhodes@users.noreply.github.com>
2023-06-24 17:53:14 -0500

    Add comment about replacing #

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -4,14 +4,18 @@ const WRAPPER = document.getElementById('wrap');
 
 // Functions
 
+///// Must replace existing squares with the specified
+
 function canvasSelector (inputNumber) {
 
   for (i = 1; i <= inputNumber; i++) {
 
     div = document.createElement('div')
     div.classList.add('pixel',`pixel-${i}`);
-    
+
     WRAPPER.appendChild(div);
   }
 
 }
+
+canvasSelector(256);
