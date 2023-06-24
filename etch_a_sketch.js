aad841237af8bf89dc83c5ddf91e1bf7521d7291
road2rhodes <132181574+road2rhodes@users.noreply.github.com>
2023-06-24 17:25:32 -0500

    Create function to choose canvas size

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,4 +1,17 @@
 // Defintions
 
+const WRAPPER = document.getElementById('wrap'); 
 
 // Functions
+
+function canvasSelector (inputNumber) {
+
+  for (i = 1; i <= inputNumber; i++) {
+
+    div = document.createElement('div')
+    div.classList.add('pixel',`pixel-${i}`);
+    
+    WRAPPER.appendChild(div);
+  }
+
+}
