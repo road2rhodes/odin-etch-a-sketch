af135dfbe6936d1e94cd1ac35a2889efeddbe5a9
road2rhodes <132181574+road2rhodes@users.noreply.github.com>
2023-06-26 16:49:42 -0500

    Remove empty space

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -8,20 +8,19 @@ const WRAPPER = document.getElementById('wrap');
 
 function canvasSelector (inputNumber) {
 
-  
-
   for (i = 1; i <= inputNumber**2; i++) {
 
-    div = document.createElement('div')
+    div = document.createElement('div');
     div.classList.add('pixel',`pixel-${i}`);
     WRAPPER.appendChild(div);
     
   }
 
-  WRAPPER.style.cssText=`grid-template-columns: repeat(${inputNumber}, 1fr); grid-template-rows: repeat(${inputNumber}, 1fr)`;
+  WRAPPER.style.cssText=`grid-template-columns: repeat(${inputNumber}, 1fr);
+  grid-template-rows: repeat(${inputNumber}, 1fr)`;
 
 
 
 }
 
-canvasSelector(16);
+canvasSelector(100);
